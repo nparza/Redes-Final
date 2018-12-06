5d5358a2c27ea9a2f0aec45873c10ca3f1851247
sofinico <nicolettisofia1@gmail.com>
2018-12-06 01:07:48 -0300

    calcular, guardar y cargar comunidad

--- clustering-graph.py
+++ clustering-graph.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Dec  4 15:14:51 2018
-
-@author: noelp
-"""
 
 import igraph as ig
 
 #%%
+
 '''
 CARGO GRAFO PRINCIPAL REPOS
 rdg: repos directed graph
 rdw: repos directed weights
 '''
-rdg = ig.read(r'C:\Users\noelp\Documents\Git\Redes-TPFinal-Grafos\Grafos\directed-users-002.gml')
+rdg = ig.read('repos-directed.gml')
 rdw = [rdg.es[n]['weight'] for n in range(rdg.ecount())]
 
 #%%
@@ -23,14 +19,30 @@ CARGO GRAFO PRINCIPAL USERS
 udg: users directed graph
 udw: users directed weights
 '''
-udg = ig.read(r'C:\Users\noelp\Documents\Git\Redes-TPFinal-Grafos\Grafos\directed-users-002.gml')
+udg = ig.read('users-directed.gml')
 udw = [udg.es[n]['weight'] for n in range(udg.ecount())]
 
 #%%
-'''
-Clusterizo
-udv creo que es una lista de listas donde cada lista es una comunidad 
+
+''' 
+CALCULO CLUSTERS CON INFOMAP
+udv/rdv creo que es una lista de listas donde cada lista es una comunidad 
 '''
 
-udv = udg.community_infomap(edge_weights=udw, vertex_weights=None, trials=50)
-#rdv = rdg.community_infomap(edge_weights=rdw, vertex_weights=None, trials=10)
+udvc = udg.community_infomap(edge_weights=udw, vertex_weights=None, trials=10)
+#rdvc = rdg.community_infomap(edge_weights=rdw, vertex_weights=None, trials=10)
+
+'''
+Ir a savecsv.py y guardar!
+'''
+
+#%%
+
+'''
+CREO OBJETO VERTEX CLUSTERING (que es lo que me devuelve la función community)
+A partir de:
+    - lista membership
+    - grafo
+'''
+
+rdvc = ig.VertexClustering(rdg, membership = membership)
